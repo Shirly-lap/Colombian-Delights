96ddb7bb07e542b12af6d9c31e54f62ab1f055a0
PablolzDev <pabloinprogramacion@gmail.com>
2024-05-26 21:02:48 -0500

    feat: View Categories

--- Colombian-Delights/src/js/HTMLcategories.js
+++ Colombian-Delights/src/js/HTMLcategories.js
@@ -1,39 +1,50 @@
-const cards = document.querySelector("#foods");
+import { restaurants } from "../js/restaurants";
 
-if (cards) {
-  let cardHTML = "";
+const categories = document.querySelector("#categories-list");
+const burger = document.querySelector(".burger-list");
+const fries = document.querySelector(".fries-list");
+const tipyc = document.querySelector(".typic-list");
+const hotdog = document.querySelector(".hotdog-list");
+const card = document.querySelector(".cardContainer"); 
 
-  for (let i = 0; i < 5; i++) {
-    cardHTML += `
-      <div class="col">
-        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" >
-          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-            <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
-            <ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">
-                <img src="https://colombian-delights.s3.us-east-2.amazonaws.com/imagen9.webp" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
-              </li>
-              <li class="d-flex align-items-center me-3">
-                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
-                <small>Earth</small>
-              </li>
-              <li class="d-flex align-items-center">
-                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
-                <small>3d</small>
-              </li>
-            </ul>
+// Agrega un controlador de eventos de clic a cada imagen de categoría
+categories.addEventListener("click", function (event) {
+    if (event.target.classList.contains("burger-list")) {
+      createCard(restaurants, 'hamburgers');
+    } else if (event.target.classList.contains("fries-list")) {
+      createCard(restaurants, 'frenchFries');
+    } else if (event.target.classList.contains("typics-list")) {
+      createCard(restaurants, 'tipicas');
+    } else if (event.target.classList.contains("hotdog-list")) {
+      createCard(restaurants, 'hotDogs');
+    }
+});
+
+function createCard(restaurants, category) {
+  let cardContainer = document.querySelector(".cardContainer");
+  let cardHTML = '';
+
+  restaurants.forEach((restaurant) => {
+    restaurant.menu[category].forEach((item) => {
+      cardHTML += `
+        <div class="shop-card">
+          <div class="title">${item.name}</div>
+          <figure data-color="#E24938, #A30F22">
+            <img src="${item.image}" class="" />
+          </figure>
+          <div class="cta">
+            <div class="desc fw-bold">${restaurant.name}</div>
+            <button class="btn">Visitar<span class="bg"></span></button>
           </div>
         </div>
-      </div>
-    `;
-  }
+      `;
+    });
+  });
 
-  cards.innerHTML = `
-    <div class="container px-4 py-5" id="custom-cards">
-      <h2 class="pb-2 border-bottom">Custom cards</h2>
-      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        ${cardHTML}
-      </div>
+  cardContainer.innerHTML = `
+    <div class="d-flex align-items-center flex-wrap gap-5">
+      ${cardHTML}
     </div>
   `;
 }
+
