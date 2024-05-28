2434790e2a8a57fcd774542a89ccd3de1051a1e9
Camilobarre <barrecami@gmail.com>
2024-05-28 14:39:56 -0400

    feat: Link database.json with js

--- Colombian-Delights/src/js/admin.js
+++ Colombian-Delights/src/js/admin.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
-	});
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
-});
+const URL_RESTAURANT = `http://localhost:3000/restaurantes`;
+let tabla = document.querySelector("#restaurantTable");
+let tbody = tabla.getElementsByTagName("tbody")[0];
+
+async function insertarDatosEnTabla(tabla) {
+  const response = await fetch(URL_RESTAURANT);
+  const restaurantes = await response.json();
+  restaurantes.forEach(restaurant => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${restaurant.id}</td>
+      <td>${restaurant.name}</td>
+      <td>${restaurant.description}</td>
+      <td>${restaurant.address}</td>
+      <td>options</td>
+    `;
+    tbody.appendChild(row);
+  });
+}
+
+// Call the function to insert data
+insertarDatosEnTabla(tabla);
