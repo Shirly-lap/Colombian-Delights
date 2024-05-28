5371d5655b33136e430bd3a318b57848db290f34
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-05-28 07:13:41 -0500

    feat: comments index.html

--- Colombian-Delights/src/js/main.js
+++ Colombian-Delights/src/js/main.js
@@ -3,3 +3,12 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+(function(d, s, id) {
+    let js;
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://embedsocial.com/cdn/ht.js";
+    d.getElementsByTagName("head")[0].appendChild(js);
+  }(document, "script", "EmbedSocialHashtagScript"));
