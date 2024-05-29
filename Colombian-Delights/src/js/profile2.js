5709ccdca2df94def8aeee5aef63b54a33ce1f7a
PablolzDev <pabloinprogramacion@gmail.com>
2024-05-29 02:33:21 -0500

    feat: Getting the id of the card

--- Colombian-Delights/src/js/profile2.js
+++ Colombian-Delights/src/js/profile2.js
@@ -1,32 +1,32 @@
-let map = L.map('map').setView([0,-1], 16);
-let customIcon = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/4244/4244731.png',
-    iconSize: [38, 38]
-});
+// let map = L.map('map').setView([0,-1], 16);
+// let customIcon = L.icon({
+//     iconUrl: 'https://cdn-icons-png.flaticon.com/512/4244/4244731.png',
+//     iconSize: [38, 38]
+// });
 
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
-// Add current location
-function addCurrentLocationMarker() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            let lat = position.coords.latitude;
-            let lng = position.coords.longitude;
-            L.marker([lat, lng]).addTo(map).bindPopup("Tu ubicación actual").openPopup();
-        });
-    } else {
-        alert("La geolocalización no es compatible con este navegador.");
-    }
-}
+// // Add current location
+// function addCurrentLocationMarker() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(function (position) {
+//             let lat = position.coords.latitude;
+//             let lng = position.coords.longitude;
+//             L.marker([lat, lng]).addTo(map).bindPopup("Tu ubicación actual").openPopup();
+//         });
+//     } else {
+//         alert("La geolocalización no es compatible con este navegador.");
+//     }
+// }
 
-// Add the position of the restaurant
-function addUserDefinedLocation(lat, lng, popupText) {
-    L.marker([lat, lng], {icon: customIcon}).addTo(map).bindPopup(popupText).openPopup();
-}
+// // Add the position of the restaurant
+// function addUserDefinedLocation(lat, lng, popupText) {
+//     L.marker([lat, lng], {icon: customIcon}).addTo(map).bindPopup(popupText).openPopup();
+// }
 
-// Add marker when page load
-addCurrentLocationMarker();
-addUserDefinedLocation(6.21018,-75.57166, "Las Delicias de Don Pedro");
+// // Add marker when page load
+// addCurrentLocationMarker();
+// addUserDefinedLocation(6.21018,-75.57166, "Las Delicias de Don Pedro");
